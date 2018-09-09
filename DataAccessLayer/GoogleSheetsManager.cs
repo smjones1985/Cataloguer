949b8af9f515a2da7473dbc8f1a1f9e2e5819c31
Sean Jones <smjones.virj@gmail.com>
2018-09-09 02:02:40 -0500

    also got get and insert working

--- DataAccessLayer/GoogleSheetsManager.cs
+++ DataAccessLayer/GoogleSheetsManager.cs
@@ -14,10 +14,25 @@ namespace DataAccessLayer
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets, SheetsService.Scope.SpreadsheetsReadonly};
         static string ApplicationName = "Google Sheets API .NET Quickstart";
 
-        private SheetsService service { get; set; }
+        private SheetsService service;
+        private SheetsService Service
+        {
+            get
+            {
+                if(service == null)
+                {
+                    EstablishCredentials();
+                }
+                return service;
+            }
+            set
+            {
+                service = value;
+            }
+        }
 
         public void EstablishCredentials()
         {
@@ -37,7 +52,7 @@ namespace DataAccessLayer
             }
 
             // Create Google Sheets API service.
-            service = new SheetsService(new BaseClientService.Initializer()
+            Service = new SheetsService(new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
                 ApplicationName = ApplicationName,
@@ -46,28 +61,63 @@ namespace DataAccessLayer
         
         }
 
-        public void SaveCatalogueRecord(object catalogueRecord)
+        public bool InsertRecord(string spreadSheetId, string range,object catalogueRecord)
         {
+            string newId = Guid.NewGuid().ToString();
+            IList<IList<Object>> valueData = new List<IList<object>>();
+            valueData.Add(new List<object>() { newId, catalogueRecord });
 
+            ValueRange insertRecord = new ValueRange();
+            insertRecord.Values = valueData;
+           
+            SpreadsheetsResource.ValuesResource.AppendRequest request =
+                    Service.Spreadsheets.Values.Append(insertRecord, spreadSheetId, range);
+
+            request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            var response = request.Execute();
+            
+            if(response.Updates.UpdatedRows != null && response.Updates.UpdatedRows > 0)
+            {
+                return true;
+            }
+            return false;
         }
+
+        public void UpdateRecord(string id, object newData)
+        {
+        }
+
 
         public IList<IList<Object>> GetData(String spreadsheetId, String range)
         {
             // Define request parameters.
-            spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-            range = "Class Data!A2:E";
+            //spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            //range = "Class Data!A2:E";
             SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
+                    Service.Spreadsheets.Values.Get(spreadsheetId, range);
 
             // Prints the names and majors of students in a sample spreadsheet:
             // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
             ValueRange response = request.Execute();
             IList<IList<Object>> values = response.Values;
-            if (values != null && values.Count > 0)
+            return values;
+        }
+
+        public void CreateSheet(string sheetName)
+        {
+            Spreadsheet spreadsheet = new Spreadsheet();
+            spreadsheet.Properties = new SpreadsheetProperties()
             {
-                return values;
+                Title = sheetName
+            };
+            SpreadsheetsResource.CreateRequest createRequest = Service.Spreadsheets.Create(spreadsheet);
+            var response = createRequest.Execute();
+            if(response == null || response.SpreadsheetId == null)
+            {
+                throw new Exception("SpreadSheet creation failed");
             }
-            return null;
         }
     }
 }
