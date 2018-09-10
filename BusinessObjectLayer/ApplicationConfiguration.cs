8bd821b9e997fa3c98015d02dcc15769c9d4fdba
Sean Jones <smjones.virj@gmail.com>
2018-09-09 21:47:04 -0500

    initial interfaces and updated working test

---
+++ BusinessObjectLayer/ApplicationConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjectLayer
+{
+    public static class ApplicationConfiguration
+    {
+        public static string SheetId { get; set; }
+    }
+}
---
+++ DataAccessLayer/IDataManager.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+    public interface IDataManager
+    {
+        void Configure(object configuration);
+
+        void InsertData(string id, object catalogueItem);
+
+        IList<object> GetDataById(string id);
+
+        void UpdateData(string id, object catalogueItem);
+
+        IList<IList<object>> GetAllData();
+
+    }
+}
